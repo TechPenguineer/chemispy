2b332beab6207f5688e9c39c31fa3276300965df
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-23 13:20:32 -0400

    added atomic query range

---
+++ elements/potassium.py
@@ -0,0 +1,7 @@
+class POTASSIUM_ELEMENT:     
+        ATOMIC_NUMBER=19
+        SYMBOL="K"
+        NAME="Potassium"
+        ATMOIC_WEIGHT=1.008
+        EPS=1 
+
---
+++ utils/atomic_number_query.py
@@ -0,0 +1,6 @@
+class query:
+    def query_atomic_number( atomic_number = 0):
+        atomic_query_range = range(1,118)
+        if atomic_number not in atomic_query_range:
+            print("Chem: Atomic number is not within the atomic query range!")
+            exit(1)
