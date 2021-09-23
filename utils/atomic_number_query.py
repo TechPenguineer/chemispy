2dfc25015f8b19483eb2a3480da9a267b543eac2
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-23 13:22:07 -0400

    fixed atomic number range threshold

--- utils/atomic_number_query.py
+++ utils/atomic_number_query.py
@@ -1,6 +1,8 @@
 class query:
     def query_atomic_number( atomic_number = 0):
-        atomic_query_range = range(1,118)
+        atomic_query_range = range(1,119)
         if atomic_number not in atomic_query_range:
             print("Chem: Atomic number is not within the atomic query range!")
             exit(1)
+            
+            
